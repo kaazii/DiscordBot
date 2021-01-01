e48a6d09eceb6c2a6f2f4afc9777658ec39319cb
kazi <uwaiskazi97@gmail.com>
2021-01-01 13:08:59 -0500

    Updates to the README, join, and leave.

--- commands/join.js
+++ commands/join.js
@@ -1,25 +1,34 @@
 module.exports = {
     name: 'join',
     description: "join a role",
+    usage: '!join <role>',
     execute(message, args) {
 
-       let { cache } = message.guild.roles;
-       let roleText = message.content.toLowerCase().trim().substring(6);
+        let { cache } = message.guild.roles;
+        let roleText = message.content.toLowerCase().trim().substring(6);
 
-       let role = cache.find(role => role.name.toLowerCase() === roleText);
-       let kennyBotRole = cache.find(role => role.name === 'KennyBot');
+        let role = cache.find(role => role.name.toLowerCase() === roleText);
+        let kennyBotRole = cache.find(role => role.name === 'KennyBot');
 
-       if (cache){
-           if (message.member.roles.cache.has(role.id)){
+        if (!args.length) {
+            let reply = `You didn't provide any arguments, ${message.author}!`; //using ``, you can use single quotes with no issue.
+            reply += `\nThe proper usage would be: \`${this.usage}\``;
+            return message.channel.send(reply);
+        }
+        else if (role === undefined) {
+            message.reply('That role does not exist.');
+        }
+        else if (cache) {
+            if (message.member.roles.cache.has(role.id)) {
                 message.reply('You already have this role');
-           }    
-           else if (role.rawPosition > kennyBotRole.rawPosition){
-               message.reply('Role can not be added - insufficient permissions');
-           }
-           else {
+            }
+            else if (role.rawPosition > kennyBotRole.rawPosition) {
+                message.reply('Role can not be added - insufficient permissions');
+            }
+            else {
                 message.member.roles.add(role);
                 message.reply('You have been added to ' + roleText);
-           }
-       }
+            }
+        }
     }
 }
--- commands/leave.js
+++ commands/leave.js
@@ -1,25 +1,34 @@
 module.exports = {
     name: 'leave',
     description: "leave a role",
+    usage: `!leave <role>`,
     execute(message, args) {
 
-       let { cache } = message.guild.roles;
-       let roleText = message.content.toLowerCase().trim().substring(7);
+        let { cache } = message.guild.roles;
+        let roleText = message.content.toLowerCase().trim().substring(7);
 
-       let role = cache.find(role => role.name.toLowerCase() === roleText);
-       let kennyBotRole = cache.find(role => role.name === 'KennyBot');
+        let role = cache.find(role => role.name.toLowerCase() === roleText);
+        let kennyBotRole = cache.find(role => role.name === 'KennyBot');
 
-       if (role){
-           if (!message.member.roles.cache.has(role.id)){ 
-               message.reply('You do not have this role');
-           }
-           else if (role.rawPosition > kennyBotRole.rawPosition){
-            message.reply('Role can not be removed - insufficient permissions');
-           }
-           else{
-            message.member.roles.remove(role);
-            message.reply('You have been removed from ' + roleText);
-           }
-       }
+        if (!args.length) {
+            let reply = `You didn't provide any arguments, ${message.author}!`; //using ``, you can use single quotes with no issue.
+            reply += `\nThe proper usage would be: \`${this.usage}\``;
+            return message.channel.send(reply);
+        }
+        else if (role === undefined) {
+            message.reply('That role does not exist.');
+        }
+        if (role) {
+            if (!message.member.roles.cache.has(role.id)) {
+                message.reply('You do not have this role');
+            }
+            else if (role.rawPosition > kennyBotRole.rawPosition) {
+                message.reply('Role can not be removed - insufficient permissions');
+            }
+            else {
+                message.member.roles.remove(role);
+                message.reply('You have been removed from ' + roleText);
+            }
+        }
     }
 }
