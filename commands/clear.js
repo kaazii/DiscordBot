768194372ce38a250aa8256d7bd4cf4eadb13c68
kazi <uwaiskazi97@gmail.com>
2021-01-02 17:30:32 -0500

    Changes to clear, and validator.

--- commands/clear.js
+++ commands/clear.js
@@ -1,27 +1,26 @@
 const validator = require("../util/validator.js");
+
+async function deleteMessages(limit, message) {
+    let results = await message.channel.messages.fetch({ limit })
+    const botMessages = results.filter(msg => msg.author.bot);
+    const commandMessages = results.filter(msg => msg.content.substring(0, 1) === '!');
+    message.channel.bulkDelete(botMessages.concat(commandMessages));
+}
 module.exports = {
     name: 'clear',
     description: "This will delete bot and user command messages from the last 50, or your input, messages from the text channel",
     usage: '[number]',
     cooldown: 3,
     async execute(message, args) {
-        let input = message.content.substring(7);
-        if (!validator.isNumericValid(input)) {
+        let input = message.content.substring(7).trim();
+        if (!args.length) {
+            await deleteMessages(50, message);
+        }
+        else if (!validator.isNumericValid(input)) {
             message.reply('Please enter an integer between 1-100, or only !clear to clear the last 50 messages.');
         }
-        else if (!args.length) {
-            let results = await message.channel.messages.fetch({ limit: 50 })
-            const botMessages = results.filter(msg => msg.author.bot);
-            const commandMessages = results.filter(msg => msg.content.substring(0, 1) === '!');
-            console.log(botMessages);
-            message.channel.bulkDelete(botMessages);
-            message.channel.bulkDelete(commandMessages);
+        else {
+            await deleteMessages(parseInt(input), message);
         }
-        let results = await message.channel.messages.fetch({ limit: parseInt(input) })
-        const botMessages = results.filter(msg => msg.author.bot);
-        const commandMessages = results.filter(msg => msg.content.substring(0, 1) === '!');
-        console.log(botMessages);
-        message.channel.bulkDelete(botMessages);
-        message.channel.bulkDelete(commandMessages);
     }
 }
--- util/validator.js
+++ util/validator.js
@@ -1,5 +1,11 @@
 module.exports = {
   isNumericValid(n) {
-      return /^\d+$/.test(n) && (!parseInt(n) > 100 || !parseInt(n) <= 0) //&& takes precedence over ||
+    return /^\d+$/.test(n) && (!parseInt(n) > 100 || !parseInt(n) <= 0) //&& takes precedence over ||
+  },
+  epochConverter(epoch) {
+    var utcSeconds = epoch / 1000;
+    var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+    d.setUTCSeconds(utcSeconds);
+    return d;
   }
 }
